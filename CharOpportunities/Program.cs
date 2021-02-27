8331a6dca96268323812d41a912bb7427538791c
Вадим Шванов <adm.shwan@gmail.com>
2021-02-27 16:46:06 +0100

    Проверка видов символов

--- CharOpportunities/Program.cs
+++ CharOpportunities/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.Text;
 
 namespace CharOpportunities
@@ -12,8 +13,20 @@ namespace CharOpportunities
 			Console.WriteLine(char.MinValue); // \0
 			Console.WriteLine(char.MaxValue); // \uffff
 
-			Console.WriteLine((int)char.MinValue); // \0
-			Console.WriteLine((int)char.MaxValue); // \uffff
+			Console.WriteLine((int)char.MinValue); // 0
+			Console.WriteLine((int)char.MaxValue); // 65535
+
+			char symbol = 'A';
+			Console.WriteLine(char.GetUnicodeCategory(symbol)); // UppercaseLetter
+
+			if (char.IsLetter(symbol))
+			{
+				Console.WriteLine("This is a letter");
+			}
+			else
+			{
+				Console.WriteLine("It is definitely not a letter");
+			}
 
 			Console.ReadLine();
 		}
