edba3722cd2bccbd2115574b56892548855de852
Вадим Шванов <adm.shwan@gmail.com>
2021-02-27 16:20:02 +0100

    Add project files.

---
+++ CharOpportunities/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace CharOpportunities
+{
+	class Program
+	{
+		static void Main()
+		{
+			Console.OutputEncoding = Encoding.Unicode;
+
+			Console.WriteLine(char.MinValue); // \0
+			Console.WriteLine(char.MaxValue); // \uffff
+
+			Console.WriteLine((int)char.MinValue); // \0
+			Console.WriteLine((int)char.MaxValue); // \uffff
+
+			Console.ReadLine();
+		}
+	}
+}
