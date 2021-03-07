ccac1b5137279e44768da7e7e24c61052808291a
Вадим Шванов <adm.shwan@gmail.com>
2021-03-07 15:10:08 +0100

    Создание строки

---
+++ StringOpportunities/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace StringOpportunities
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			CreateString();
+
+			Console.ReadLine();
+		}
+
+		private static void CreateString()
+		{
+			Console.WriteLine("это литерал"); // Литеральная строка
+
+			//string error = new String("CODE BLOG"); // Ошибка
+			string str = "CODE BLOG";
+			Console.WriteLine(str);
+
+			unsafe
+			{
+				fixed (char* pointer = str)
+				{
+					string strFromPointer = new String(pointer);
+					Console.WriteLine(strFromPointer);
+				}
+			}
+		}
+	}
+}
